fb0c641bb99b4fddaf216de71b2ae6b5fb11641e
Peter Grace <petegrace@stackblitz.com>
2025-05-14 11:36:28 -0400

    genrealize project name in consts

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,5 @@
 pub const API_VER: &str = "/api/v1";
-pub const PREFIX_NAMESPACE: &str = "rust-web-template";
+pub const PREFIX_NAMESPACE: &str = "{{project-name}}";
 pub const JWT_SECRET: &str = "secret";
 pub const SESSION_INACTIVITY_LIMIT_HOURS: i64 = 24;
 
